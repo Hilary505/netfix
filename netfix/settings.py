dfca4d72428d874dd592f07e7d916f95dbe1dd31
Hilary505 <oduorhilary001@gmail.com>
2024-12-19 04:51:44 +0300

    feat(netfix(settings.py)):  Add static files directory and redirects

--- netfix/settings.py
+++ netfix/settings.py
@@ -123,4 +123,15 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+import os.path
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR,'static')
+]
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+]
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
