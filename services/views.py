4d0e3139fb6817e2664ec1f3a6a1e43fcc0f73ff
hilaromondi <omondihilary19@gmail.com>
2025-01-10 10:14:41 +0300

    feat(services(views.py)): Add code  for  function request_service

--- services/views.py
+++ services/views.py
@@ -30,4 +30,26 @@ def service_field(request, field):
 
 
 def request_service(request, id):
-    return render(request, 'services/request_service.html', {})
+    service = get_object_or_404(Service, id=id)
+    customer = get_object_or_404(Customer, user=request.user)  # Get the logged-in customer
+    
+    if request.method == 'POST':
+        form = RequestServiceForm(request.POST)
+        if form.is_valid():
+            service_hours = form.cleaned_data['service_hours']
+            address = form.cleaned_data['address']
+            total_price = service.price_hour * service_hours  # Calculate total price
+
+            # Save the service request
+            ServiceRequest.objects.create(
+                customer=customer,
+                service=service,
+                service_hours=service_hours,
+                address=address,
+                total_price=total_price
+            )
+            return redirect('customer_profile', username=request.user.username)  # Redirect after success
+    else:
+        form = RequestServiceForm()
+
+    return render(request, 'services/request_service.html', {'form': form, 'service': service})
